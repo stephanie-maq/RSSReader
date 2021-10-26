d71839e7acd900c7800f4880a3cc8981e42f28c2
Ike <ike.okoroha@hotmail.com>
2021-10-26 21:45:55 +0200

    Serializer klass gjord

---
+++ DataAccessLayer/SerializerForXml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using Models;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    internal class SerializerForXml
+    {
+        public void Serialize(List<Podcast> podcastList)
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Podcast>));
+                using (FileStream outFile = new FileStream("Podcasts.xml", FileMode.Create,
+                    FileAccess.Write))
+                {
+                    xmlSerializer.Serialize(outFile, podcastList);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public List<Podcast> Deserialize()
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Podcast>));
+                using (FileStream inFile = new FileStream("Podcasts.xml", FileMode.Open,
+                    FileAccess.Read))
+                {
+                    return (List<Podcast>)xmlSerializer.Deserialize(inFile);
+                }
+            }
+            catch (Exception e)
+            {
+                
+                throw new Exception(e.Message); 
+                
+            }
+        }
+    }
+}
